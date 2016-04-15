c0d5e08803106db3cffb1e3cc0079360d2000d1e
Palmer Harrell <palmerharrell@me.com>
2016-04-15 15:11:30 -0500

    Created Species classes

---
+++ GalaxyWars/Species/NASA.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaxyWars.Species.Interfaces;
+
+namespace GalaxyWars.Species
+{
+  class NASA : Species, ISpaceFaring
+  {
+    public bool wearsLabCoat { get; set; }
+    public int iq { get; set; }
+    public string researchType { get; set; }
+
+    public string averageBloodPressure { get; set; }
+    public bool receivesGovFunding { get; set; }
+
+    public NASA()
+    {
+      population = 1000000;
+      allowedVehicle = "Space Bicycle";
+      affiliation = "Science";
+    }
+
+    public NASA(string planet)
+    {
+      homePlanet = planet;
+      population = 1000000;
+      allowedVehicle = "Space Bicycle";
+      affiliation = "Science";
+    }
+  }
+}
---
+++ GalaxyWars/Species/SpacePope.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaxyWars.Species.Interfaces;
+
+namespace GalaxyWars.Species
+{
+  class SpacePope : Species, IReligious
+  {
+    public int faithRating { get; set; }
+    public int autoHealRate { get; set; }
+    public string denomination { get; set; }
+
+    public string averageHeight { get; set; }
+    public bool hasTallHats { get; set; }
+
+    public SpacePope()
+    {
+      population = 1000000;
+      allowedVehicle = "Galactic Chariot";
+      affiliation = "Religion";
+    }
+
+    public SpacePope(string planet)
+    {
+      homePlanet = planet;
+      population = 1000000;
+      allowedVehicle = "Galactic Chariot";
+      affiliation = "Religion";
+    }
+  }
+}
