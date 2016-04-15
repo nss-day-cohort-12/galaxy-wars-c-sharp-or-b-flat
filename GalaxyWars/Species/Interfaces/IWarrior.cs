734dd63071065e78cdf367e06bb548003c0f4ebd
Palmer Harrell <palmerharrell@me.com>
2016-04-15 14:43:33 -0500

    Added 3 interfaces

---
+++ GalaxyWars/Species/Interfaces/IWarrior.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyWars.Species.Interfaces
+{
+  interface IWarrior
+  {
+    int strength { get; set; }
+    int armorRating { get; set; }
+    string weapon { get; set; }
+  }
+}
