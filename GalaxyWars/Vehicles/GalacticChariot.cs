415713f2ee829b52afe7d2a48c5194608ff9b6bf
Palmer Harrell <palmerharrell@me.com>
2016-04-15 13:54:58 -0500

    Added vehicle classes

---
+++ GalaxyWars/Vehicles/GalacticChariot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyWars.Vehicles
+{
+  class GalacticChariot : Vehicle
+  {
+    public bool hasSpaceHorses = true;
+    public bool isAerodynamic = false;
+    public bool isCosmodynamic = true;
+
+    public override void accelerate()
+    {
+      Console.WriteLine("The chariot is accelerating.");
+    }
+
+    public GalacticChariot()
+    {
+      name = "Galactic Chariot";
+      allowedDriverAffiliation = "Religion";
+    }
+  }
+}
